3781c292ff9d3d9dd46da564e02fc67cebf2499e
DemetreShonia <shonka.demeloper@gmail.com>
2022-09-05 16:35:46 +0400

    Filter Stuff

--- react-tutor/src/App.js
+++ react-tutor/src/App.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./Components/Expenses";
 import ExpenseItem from "./Components/Expenses/ExpenseItem";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car insurance",
+    amount: 10,
+    date: new Date(2002, 2, 12),
+  },
+  {
+    id: "e2",
+    title: "Car insurance 2",
+    amount: 11,
+    date: new Date(2001, 3, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      title: "Car insurance",
-      amount: 10,
-      date: new Date(2002, 2, 12),
-    },
-    {
-      title: "Car insurance 2",
-      amount: 11,
-      date: new Date(2001, 3, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log("in APP JS");
-    console.log(expenses);
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   };
   return (
     <div className="App">
--- react-tutor/src/Components/Expenses.js
+++ react-tutor/src/Components/Expenses.js
@@ -2,12 +2,17 @@ import ExpenseItem from "../Components/Expenses/ExpenseItem";
 import "./Expenses.css";
 import Card from "../Components/UI/Card";
 import ExpensesFilter from "./Expenses/ExpensesFilter";
+import ExpensesList from "./Expenses/ExpensesList";
 import { useState } from "react";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter(
+    (e) => e.date.getFullYear() == filteredYear
+  );
+
   return (
     <div>
       <Card className="expenses">
@@ -15,16 +20,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          date={props.items[0].date}
-          amount={props.items[0].amount}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          date={props.items[1].date}
-          amount={props.items[1].amount}
-        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
